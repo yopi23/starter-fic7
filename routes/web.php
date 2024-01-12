b9f389069c5c90943d4d0dce993b43865106c49b
yopi <yopiversi3@gmail.com>
2024-01-12 11:12:13 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('blank-page', ['type_menu' => '']);
 });
+// Route::get('/login', function () {
+//     return view('auth.login');
+// });
+// Route::get('/register', function () {
+//     return view('auth.register');
+// });
+// Route::get('/reset', function () {
+//     return view('auth.reset');
+// });
+// Route::get('/forgot', function () {
+//     return view('auth.forgot');
+// });
+// Route::get('/verify', function () {
+//     return view('auth.verify');
+// });
